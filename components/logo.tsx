74711cc91b830f01a5a6dcd869d3228cde484d66
mvrzvkot <jakubpiaszczyk5017@gmail.com>
2025-12-01 15:18:43 +0100

    Update logo.tsx

--- components/logo.tsx
+++ components/logo.tsx
@@ -2,7 +2,7 @@ export function Logo() {
   return (
     <div className="flex items-center gap-3">
       <img 
-        src="/logo.png" 
+        src="/EK-logo.png" 
         alt="Ewelina Kijewska Kancelaria Adwokacka" 
         className="h-12 w-auto"
       />
